23127404fa9f709be7786913259cddb02e640da2
Federico Gobea <65693362+fedegobea@users.noreply.github.com>
2022-09-29 14:27:29 -0300

    🎉 Initial commit

---
+++ test_addon/__manifest__.py
@@ -0,0 +1,14 @@
+
+{
+    'name': 'Test addon',
+    'version': '14.0.21.13',
+    'author': 'Ejemplo',
+    'website': 'https://www.ejemplo.com',
+    "category": "Sales",
+    "depends": [],
+    'data': [],
+    'demo_xml': [],
+    'installable': True,
+    'application': True,
+
+}
